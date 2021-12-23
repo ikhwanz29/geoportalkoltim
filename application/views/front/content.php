902c97f66c61b1a2151f3db11d2ef33dbd2ec721
Ikhwanz <ikhwanuluzlahtkj@gmail.com>
2021-12-23 10:04:33 +0800

    Menambahkan Header dan Icon Kartografis

--- application/views/front/content.php
+++ application/views/front/content.php
@@ -1,42 +1,182 @@
-    <main id="main">
+<main id="main">
 
-        <!-- ======= peta Section ======= -->
-        <section id="peta" class="services section-bg">
-            <div class="container" data-aos="fade-up">
+  <!-- ======= Peta Section ======= -->
+  <section id="peta" class="services section-bg">
+    <div class="container" data-aos="fade-up">
+      <div class="section-title">
+        <h2>Kategori Kartografis</h2>
+      </div>
 
-                <div class="section-title">
-                    <h2>Servis dan Peta</h2>
-                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-                </div>
-
+      <div class="row">
+        <!--Icon Box Start-->
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#batas_wilayah">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/batas wilayah.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                BATAS WILAYAH
+              </h5>
             </div>
-        </section><!-- End peta Section -->
+          </a>
+        </div>
 
-        <!-- ======= Berita Section ======= -->
-        <section id="berita" class="skills">
-            <div class="container" data-aos="fade-up">
-
-                <div class="section-title">
-                    <h2>Berita</h2>
-                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-                </div>
-
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#dataset_khusus">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/dataset khusus.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                DATASET KHUSUS
+              </h5>
             </div>
-        </section><!-- End Berita Section -->
+          </a>
+        </div>
 
-        <!-- ======= Team Section ======= -->
-        <section id="team" class="team section-bg">
-            <div class="container" data-aos="fade-up">
-
-                <div class="section-title">
-                    <h2>Team</h2>
-                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-                </div>
-
-
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#geologi">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/geologi.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                GEOLOGI
+              </h5>
             </div>
-        </section><!-- End Team Section -->
+          </a>
+        </div>
 
-    </main><!-- End #main -->
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#hidrografi">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/hidrografi.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                HIDROGRAFI
+              </h5>
+            </div>
+          </a>
+        </div>
 
- 
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#hipsografi">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/hipsografi.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                HIPSOGRAFI
+              </h5>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#kadaster">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/kadaster.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                KADASTER
+              </h5>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#kebencanaan">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/kebencanaan.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                KEBENCANAAN
+              </h5>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#lingkungan_terbangun">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/lingkungan terbangun.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                LINGKUNGAN TERBANGUN
+              </h5>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#referensi_spesial">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/referensi spesial.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                REFERENSI SPESIAL
+              </h5>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#tanah">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/tanah.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                TANAH
+              </h5>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#toponimi">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/toponimi.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                TOPONIMI
+              </h5>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#transportasi">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/transportasi.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                TRANSPORTASI
+              </h5>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#utilitas">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/utilitas.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                UTILITAS
+              </h5>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-6 col-sm-6 col-md-3 col-lg-2 text-center kartografis-item">
+          <a class="kartografis" target="_blank" href="#vegetasi">
+            <img class="kartografis-img" style="cursor: pointer;" src="<?= base_url('assets/icon/vegetasi.png'); ?>" width="135px" height="135px">
+            <div class="kartografis-text">
+              <h5 class="kartografis-title" style="color: #444444; font-weight: bold;">
+                VEGETASI
+              </h5>
+            </div>
+          </a>
+        </div>
+        <!--Icon Box End-->
+      </div>
+    </div>
+  </section><!-- End Peta Section -->
+
+  <!-- ======= Berita Section ======= -->
+  <section id="berita" class="skills">
+    <div class="container" data-aos="fade-up">
+
+      <div class="section-title">
+        <h2>Berita</h2>
+        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+      </div>
+
+    </div>
+  </section><!-- End Berita Section -->
+
+</main><!-- End #main -->
--- application/views/front/template.php
+++ application/views/front/template.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Geoportal - <?= $title;?></title>
+    <title>Geoportal - <?= $title; ?></title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -45,15 +45,13 @@
             <h1 style="color: white; font-weight: bold;" class="logo me-auto">GEOPORTAL KOLAKA TIMUR</h1>
             <!-- Uncomment below if you prefer to use an image logo -->
             <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
             <nav id="navbar" class="navbar">
                 <ul>
                     <li><a style="font-weight: bold; font-size: 16px;" class="nav-link scrollto active" href="#hero">Home</a></li>
-                    <li><a style="font-weight: bold; font-size: 16px;" class="nav-link scrollto" href="#peta">Servis dan Peta</a></li>
+                    <li><a style="font-weight: bold; font-size: 16px;" class="nav-link scrollto" href="#peta">Kategori Kartografis</a></li>
                     <li><a style="font-weight: bold; font-size: 16px;" class="nav-link scrollto" href="#berita">Berita</a></li>
-                    <li><a style="font-weight: bold; font-size: 16px;" class="nav-link scrollto" href="#team">Team</a></li>
                     <li><a style="font-weight: bold; font-size: 16px;" class="nav-link scrollto" href="#kontak">Kontak</a></li>
-               
+
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
             </nav><!-- .navbar -->
@@ -62,33 +60,31 @@
     </header><!-- End Header -->
 
     <!-- ======= Hero Section ======= -->
-    <section id="hero" class="d-flex align-items-center">
-
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-                    <h1>Selamat Datang</h1>
-                    <h2>Di Jaringan Informasi Geospasial Daerah (JIGD), 
-                        Kabupaten Kolaka Timur</h2>
-                    <div class="d-flex justify-content-center justify-content-lg-start">
-                        <a href="#peta" class="btn-get-started scrollto">Get Started</a>
-                    </div>
+    <section id="hero">
+        <div class="hero">
+            <div class="banner">
+                <br><br>
+                <div class="banner_overlay">
+                    <img src="<?= base_url('assets/KantorBupati.jpg'); ?>" width="100%" height="450px" alt="">
                 </div>
-                <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-                    <img src="<?= base_url(); ?>assets/Koltim.png" class="img-fluid animated" alt="">
+                <div class="hero_paragraf">
+                    <div class="container">
+                        <!-- <p class="hero_paragraf_teks">
+                            <b>SELAMAT DATANG</b> di JIGD Kabupaten Kolaka Timur
+                        </p> -->
+                    </div>
                 </div>
             </div>
         </div>
-
     </section><!-- End Hero -->
 
     <!-- Isi disimpan disini -->
-    <?php echo $contents;?>
+    <?php echo $contents; ?>
     <!-- End isi disimpan disini -->
 
     <!-- ======= Footer ======= -->
     <footer id="footer">
-        
+
         <!-- ======= Kontak Section ======= -->
         <section id="kontak" class="skills">
             <div class="container" data-aos="fade-up">
@@ -103,7 +99,7 @@
                             Daerah (JIGD) Kabupaten Kolaka Timur yang menjadi<br>
                             mitra penghubung simpul Jaringan Informasi<br>
                             Geospasial
-                            Nasional (JIGN), dan Jaringan 
+                            Nasional (JIGN), dan Jaringan
                             Informasi Geospasial Daerah (JIGD) Provinsi Sulawesi Tenggara,
                             terhubung pada Tahun 2021.
                         </p>
@@ -125,14 +121,15 @@
 
                     <div class="col-lg-3 col-md-6 footer-contact">
                         <h3 style="color: white; font-weight: bold;">DIDUKUNG OLEH</h3>
-                        <center><img src="<?= base_url(); ?>assets/Badan Informasi Geospasial.png" width="175px" height="128px"><center>
-                        <p style="text-align: left; margin-top: 15px; font-size: 16px; color: white;">
-                        Jalan Raya Jakarta - Bogor KM. 46 Cibinong 16911, INDONESIA<br><br>
-                        Telp: 021-8753155 atau 021-8752062 ext.3608/3611/3103<br><br>
-                        Fax: 021-87908988/87916647<br><br>
-                        Email: info@big.go.id<br><br>
-                        www.big.go.id
-                        </p>
+                        <center><img src="<?= base_url(); ?>assets/Badan Informasi Geospasial.png" width="175px" height="128px">
+                            <center>
+                                <p style="text-align: left; margin-top: 15px; font-size: 16px; color: white;">
+                                    Jalan Raya Jakarta - Bogor KM. 46 Cibinong 16911, INDONESIA<br><br>
+                                    Telp: 021-8753155 atau 021-8752062 ext.3608/3611/3103<br><br>
+                                    Fax: 021-87908988/87916647<br><br>
+                                    Email: info@big.go.id<br><br>
+                                    www.big.go.id
+                                </p>
                     </div>
 
                 </div>
